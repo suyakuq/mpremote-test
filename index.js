1bf9fbc1e1d044ccf2d4027c4806c2e7f7b187cd
marco <suyakuq@gmail.com>
2016-12-02 12:07:17 +0100

    nettoyage de code

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const electron = require('electron');
 const app = electron.app;  
 const BrowserWindow = electron.BrowserWindow;
 var ipc = require('electron').ipcMain;
-var MPD = require('node-mpd');
-var mpd = new MPD({});
-global.sharedObject = {"currentSongs" : []};
+//var MPD = require('node-mpd');
+//var mpd = new MPD({});
+//global.sharedObject = {};
 let mainWindow;
 
 
@@ -15,70 +15,14 @@ app.on('window-all-closed', function() {
     app.quit();
   }
 });
-
 app.on('ready', function() {  
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.openDevTools(); //F12 equivalent
-  mpd.on("ready", function() {
-    console.log('ready');
-    for(var num = 0; num < mpd.playlist.length; num++) {
-      global.sharedObject.currentSongs.push({"pos": num, "fileName": mpd.playlist[num].file});
-      //console.log(n + ": " + mpd.playlist[num].artist + " - " + mpd.playlist[num].title);
-    }
-  });
 
-  mpd.on("update", function(status) {
-    console.log("Update:", status);
-    switch(status) {
-      case "mixer":
-      case "player":
-      case "options":
-        console.log("Status after update", mpd.status);
-        break;
-      case "playlist":
-        console.log("Playlist after update", mpd.playlist);
-        break;
-      case "database":
-        console.log("Songs after update", mpd.songs);
-        break;
-    }
-  });
-  //La Connection commence ici
-  mpd.connect();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
-  });
-
-  //Evenements et interaction avec MPD
-  ipc.on('next', function () {
-    mpd.next();
-  });
-
-  ipc.on('prev', function () {
-    mpd.prev();
-  });
-
-  ipc.on('pause', function () {
-    mpd.pause();
-  });
-
-  ipc.on('play', function () {
-    mpd.play();
-  });
-
-  ipc.on('stop', function () {
-    mpd.stop();
-  });
-
-  ipc.on('playAt', function (event, data) {
-    mpd.playAt(data.pos);
-  });
-
-  ipc.on('clear', function () {
-    mpd.clear();
-    mpd.updateStatus()
   });
 
 });
--- js/index.js
+++ js/index.js
@@ -5,73 +5,107 @@
 
 var PLAYER = {
     modules: {},
-    ipc : require('electron').ipcRenderer
+    ipc : require('electron').ipcRenderer,
+    remote : require('electron').remote,
+    MPD : require('node-mpd')
 };
 
-PLAYER.modules.actions = (function(){
+PLAYER.modules.mpd = (function () {
 
+    var mpd = new PLAYER.MPD({});
+    mpd.on("ready", function () {
+        PLAYER.modules.playlist.loadSongs(mpd.playlist);
+        console.log('ready');
+    });
+    mpd.on("update", function(status) {
+        console.log("Update:", status);
+        switch(status) {
+            case "mixer":
+            case "player":
+            case "options":
+                console.log("Status after update", mpd.status);
+                break;
+            case "playlist":
+                console.log("Playlist after update", mpd.playlist);
+                break;
+            case "database":
+                console.log("Songs after update", mpd.songs);
+                break;
+        }
+    });
     return {
-          prev: function () {
-              PLAYER.ipc.send('prev');
-          },
-          next: function () {
-              PLAYER.ipc.send('next');
-          },
-          play: function () {
-              PLAYER.ipc.send('play');
-          },
-          pause: function () {
-              PLAYER.ipc.send('pause');
-          },
-          stop: function () {
-              PLAYER.ipc.send('stop');
-          },
-            clear: function () {
-                PLAYER.ipc.send('clear');
-            }
-    };
+        init: function () {
+            mpd.connect();
+        },
+        play: function () {
+            mpd.play();
+        },
+        next: function () {
+            mpd.next();
+        },
+        prev: function () {
+            mpd.prev();
+        },
+        stop: function () {
+            mpd.stop();
+        },
+        clear: function () {
+            mpd.clear(function () {
+                mpd._updatePlaylist(PLAYER.modules.playlist.loadSongs(mpd.playlist))
+            } );
+        },
+        playAt: function (pos) {
+                mpd.playAt(pos);
+        },
+        add: function () {
+            mpd.add();
+        }
+    }
 })();
 
 PLAYER.modules.playlist = (function () {
-    var currentSongs = require('electron').remote.getGlobal('sharedObject').currentSongs;
-    console.log(currentSongs);
     return{
-        loadSongs: function () {
+        loadSongs: function (songs) {
+            console.log("songs");
+            console.log(songs);
             var ul = $('#songList');
-            for(var i = 0; i< currentSongs.length; i++){
-                ul.append($('<li>'+currentSongs[i].fileName+'</li>').attr('id', currentSongs[i].pos));
+            ul.empty();
+            for(var i = 0; i< songs.length; i++){
+                ul.append($('<li>'+songs[i].file+'</li>').attr('id', i));
             }
         },
-        playSong: function(pos){
-            console.log(pos);
-            PLAYER.ipc.send('playAt',{'pos': pos});
+        addFolder: function(name){
+               // PLAYER.ipc.send('add',{'name':name});
         },
         initSongEvents: function () {
             $('#songList').on('click', 'li', function () {
-                PLAYER.modules.playlist.playSong($(this).attr('id'));
+                PLAYER.modules.mpd.playAt($(this).attr('id'));
             });
+            $('#add').on('click', function () {
+                PLAYER.modules.playlist.addFolder($('#folder').val());
+                PLAYER.modules.playlist.loadSongs();
+            })
         }
     }
 })();
 
 
 PLAYER.modules.app = (function(){
-    var actions = PLAYER.modules.actions;
-    var playlist = PLAYER.modules.playlist;
+    var mpd = PLAYER.modules.mpd;
     return{
         start : function () {
-            playlist.loadSongs();
-            $('#prev').click(actions.prev);
-            $('#next').click(actions.next);
-            $('#play').click(actions.play);
-            $('#pause').click(actions.pause);
-            $('#stop').click(actions.stop);
-            $('#clear').click(actions.clear);
+            $('#prev').click(mpd.prev);
+            $('#next').click(mpd.next);
+            $('#play').click(mpd.play);
+            $('#pause').click(mpd.pause);
+            $('#stop').click(mpd.stop);
+            $('#clear').click(mpd.clear);
         }
     }
 
 })();
 $(document).ready(function(){
+    PLAYER.modules.mpd.init();
     PLAYER.modules.app.start();
     PLAYER.modules.playlist.initSongEvents();
 });
