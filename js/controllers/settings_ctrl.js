81cd6cd8109c4106c486764ce37d3191dc21e309
marco <suyakuq@gmail.com>
2017-03-15 12:39:07 +0100

    -Aded Settings button -Fixed connect/disconnect

--- js/controllers/settings_ctrl.js
+++ js/controllers/settings_ctrl.js
@@ -6,29 +6,25 @@ function settings_ctrl($scope, $location, MPDService) {
     $scope.player = MPDService.getPlayer();
     $scope.servers = [];
 
-        $scope.$on('onConnect', function(event, data){
+    const defaultIp = '192.168.43.10';
+    const defaultPort = 6600;
+
+    $scope.connect = function (connectionParams) {
+        var ip = (connectionParams && connectionParams.host) ? connectionParams.host : defaultIp;
+        var port = (connectionParams && connectionParams.port) ? connectionParams.port : defaultPort;
+        MPDService.connect(ip, port, function (data) {
             $scope.player = data;
             $scope.isConnected = true;
             $location.path('/')
         });
+    };
 
-        $scope.$on('onDisconnect', function(event, data){
-            $scope.player = null;
-            $scope.isConnected = false;
-        });
-
-        const defaultIp = '192.168.43.98';
-        const defaultPort = 6600;
-
-        $scope.connect = function (connectionParams) {
-            var ip = (connectionParams && connectionParams.host) ? connectionParams.host : defaultIp;
-            var port = (connectionParams && connectionParams.port) ? connectionParams.port : defaultPort;
-            MPDService.connect(ip, port);
-        };
-
-        $scope.disconnect = function () {
-          MPDService.disconnect();
-        };
+    $scope.disconnect = function () {
+      MPDService.disconnect(function () {
+          $scope.player = null;
+          $scope.isConnected = false;
+      });
+    };
 }
 
 module.exports = settings_ctrl;
